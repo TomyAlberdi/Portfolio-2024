de8454f192feaab9869406b4668dc9cc401c98c3
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-02-22 19:21:27 -0300

    Set Router Basic Structure and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import "./App.scss";
+import CustomRouter from './routes';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      eyo
+      <CustomRouter />
     </div>
   );
 }
---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import { Navigate, useRoutes } from "react-router-dom"
+import BasicLayout from './layout/BasicLayout';
+import Home from './pages/Home';
+
+const CustomRouter = () => {
+  return useRoutes([
+    {
+      path: "/",
+      element: <BasicLayout />,
+      children: [{
+        index: true,
+        element: <Home />
+      }]
+    },
+    {
+      path: "*",
+      element: <Navigate to={"/"} />
+    }
+  ])
+}
+
+export default CustomRouter
