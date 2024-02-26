d212194ce0fcff92437f398002b6c9780d929e2b
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-02-26 13:46:56 -0300

    Added Navbar & Basic Page Structure

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './../components/Navbar/Navbar';
 
 const BasicLayout = () => {
   return (
     <div className='Layout'>
+      <Navbar />
       <Outlet />
     </div>
   )
