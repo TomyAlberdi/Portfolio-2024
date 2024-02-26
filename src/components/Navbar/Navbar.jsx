2ab2a572476838a2280fabe504a5c2cc62fe6e4a
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-02-26 14:31:51 -0300

    Finished Navbar basic structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>Navbar</div>
+    <div className='Navbar'>
+      <section className="header">
+        <img src="public/favicon.ico" alt="Tomás Alberdi Icon" />
+        <div>
+          <h2>Tomás Alberdi</h2>
+          <h4>FullStack Developer</h4>
+        </div>
+      </section>
+      <nav>
+        <Link className='active'>Home</Link>
+        <Link>Projects</Link>
+        <Link>Experience</Link>
+        <Link>Tech Stack</Link>
+        <Link>Contact</Link>
+      </nav>
+    </div>
   )
 }
 
