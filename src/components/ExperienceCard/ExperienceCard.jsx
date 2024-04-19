889fc951ffa47741f16d24f58fb40cb01d3f320e
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-04-19 20:03:25 -0300

    Started Experience Card description/gallery section

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import CardTitle from "./components/CardTitle";
+import CardDescriptionImages from './components/CardDescriptionImages';
 
 const ExperienceCard = ({ data }) => {
   const { t, i18n } = useTranslation();
@@ -11,8 +12,8 @@ const ExperienceCard = ({ data }) => {
   return (
     <div className="ExperienceCard" style={{ backgroundColor: bkgColor }}>
       <CardTitle name={en ? data.name_en : data.name_es} year={data.year} wip={data.wip} accentColor={accentColor} />
-      <section className="position div2">aa</section>
-      <section className="description div3"></section>
+      <section className="position div2">{en ? data.role_en : data.role_es}</section>
+      <CardDescriptionImages description={en ? data.description_en : data.description_es} images={[]} />
       <section className="stack div4"></section>
       <section className="link div5"></section>
     </div>
---
+++ src/components/ExperienceCard/components/CardDescriptionImages.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CardDescriptionImages = ({description, images}) => {
+  return (
+    <section className="div3 description">
+      <div className="description">
+        {description}
+      </div>
+      <div className="images">
+        gallery
+      </div>
+    </section>
+  )
+}
+
+export default CardDescriptionImages
