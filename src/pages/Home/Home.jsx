08a5a4c57d1f46c6fa0c0d5082f2416034673fb0
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-04-25 18:15:07 -0300

    Completed Home Page Rework

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,110 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import ReactCardFlip from "react-card-flip";
-import { useTranslation } from "react-i18next";
-import TextTransition, { presets } from "react-text-transition";
+import { useTranslation, Trans } from "react-i18next";
 
 const Home = () => {
   const { t, i18n } = useTranslation();
-
-  const [index, setIndex] = useState(0);
-  const [index1, setIndex1] = useState(0);
-  const [index2, setIndex2] = useState(0);
-  const [index3, setIndex3] = useState(0);
-  const [index4, setIndex4] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => setIndex((index) => index + 1), 4000);
-    const intervalIcon1 = setInterval(
-      () => setIndex1((index1) => index1 + 1),
-      4000
-    );
-    const intervalIcon2 = setInterval(
-      () => setIndex2((index2) => index2 + 1),
-      4000
-    );
-    const intervalIcon3 = setInterval(
-      () => setIndex3((index3) => index3 + 1),
-      4000
-    );
-    const intervalIcon4 = setInterval(
-      () => setIndex4((index4) => index4 + 1),
-      4000
-    );
-    return () =>
-      clearTimeout(
-        intervalId,
-        intervalIcon1,
-        intervalIcon2,
-        intervalIcon3,
-        intervalIcon4
-      );
-  }, []);
-
-  const texts_en = [
-    "Frontend Developer",
-    "Backend Developer",
-    "Database Administrator",
-  ];
-  const texts_es = [
-    "Desarrollador Frontend",
-    "Desarrollador Backend",
-    "Administrador de BBDD",
-  ];
-
-  const icons_1 = [
-    <i class="fa-brands fa-react"></i>,
-    <i class="fa-solid fa-database"></i>,
-    <i class="fa-brands fa-java"></i>,
-  ];
-  const icons_2 = [
-    <i class="fa-brands fa-js"></i>,
-    <i class="fa-brands fa-docker"></i>,
-    <i class="fa-brands fa-golang"></i>,
-  ];
-  const icons_3 = [
-    <i class="fa-brands fa-sass"></i>,
-    <i class="fa-solid fa-code-compare"></i>,
-    <i class="fa-solid fa-circle-nodes"></i>,
-  ];
-  const icons_4 = [
-    <i class="fa-brands fa-html5"></i>,
-    <i class="fa-solid fa-file-lines"></i>,
-    <i class="fa-solid fa-lock"></i>,
-  ];
+  const Español = i18n.language === "es";
 
   return (
     <div className="RightPanel Home">
       <section className="div1">
-        <img src="public/favicon.ico" />
-        <div>Tomás <br /> Alberdi</div>
+        {Español ? (
+          <p>
+            Nacido en 2001 en <span>La Plata</span>, Argentina. <br />
+            Con el tiempo, he adquirido un conjunto diverso de habilidades
+            <span> técnicas y sociales </span> que estoy seguro contribuirán a
+            mi desarrollo como profesional en este campo.
+          </p>
+        ) : (
+          <p>
+            Born in 2001 in <span>La Plata</span>, Argentina. <br />
+            Over the years, I have acquired a diverse set of
+            <span> technical and soft skills</span> that I am confident will
+            contribute to my development as a professional in the field.
+          </p>
+        )}
       </section>
       <section className="div2">
-        <div className="row icon_transition_first_row mult">
-          <TextTransition springConfig={presets.gentle} className="icon">
-            {icons_1[index1 % icons_1.length]}
-          </TextTransition>
-          <TextTransition springConfig={presets.gentle} className="icon">
-            {icons_2[index2 % icons_2.length]}
-          </TextTransition>
-        </div>
-        <div className="row text_transition_container">
-          <TextTransition springConfig={presets.gentle}>
-            {i18n.language === "en"
-              ? texts_en[index % texts_en.length]
-              : texts_es[index % texts_es.length]}
-          </TextTransition>
-        </div>
-        <div className="row icon_transition_second_row mult">
-          <TextTransition springConfig={presets.gentle} className="icon">
-            {icons_3[index3 % icons_3.length]}
-          </TextTransition>
-          <TextTransition springConfig={presets.gentle} className="icon">
-            {icons_4[index4 % icons_4.length]}
-          </TextTransition>
-        </div>
+        {Español ? (
+          <ul>
+            <li>De 2020 a 2023, cursé la <span>Licenciatura en Sistemas Informáticos</span> en la Facultad de Informática de la Universidad Nacional de La Plata.</li>
+            <li>
+              En 2023, me gradué como{" "}
+              <Link
+                to="https://www.digitalhouse.com/ar/productos/programacion/certified-tech-developer"
+                target="_blank"
+              >
+                Certified Tech Developer
+              </Link>{" "}
+              en Digital House, una carrera respaldada por Globant y Mercado
+              Libre.
+            </li>
+            <li>
+              En 2024, finalicé mis estudios en Digital House obteniendo la
+              certificación de <span>Especialista en Backend</span>.
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>From 2020 to 2023, I pursued a <span>Bachelor's degree in Computer Systems</span> in the Faculty of Informatics of the National University of La Plata.</li>
+            <li>
+              In 2023, I graduated as a{" "}
+              <Link
+                to="https://www.digitalhouse.com/ar/productos/programacion/certified-tech-developer"
+                target="_blank"
+              >
+                Certified Tech Developer
+              </Link>{" "}
+              in Digital House, a degree backed by Globant and Mercado Libre.{" "}
+            </li>
+            <li>
+              In 2024, I concluded my studies at Digital House by earning the{" "}
+              <span>Backend Specialist</span> certification.
+            </li>
+          </ul>
+        )}
       </section>
-      <section className="div3"></section>
     </div>
   );
 };
